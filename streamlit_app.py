6a089400e664622f4d43a9a387563ad7019cc5d8
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-15 12:47:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,6 +78,21 @@ def main():
             pref_ports
         )
 
+    import searoute as sr
+
+    #Define origin and destination points:
+    origin = [0.3515625, 50.064191736659104]
+
+    destination = [117.42187500000001, 39.36827914916014]
+
+
+    route = sr.searoute(origin, destination)
+    # > Returns a GeoJSON LineString Feature
+    # show route distance with unit
+    st.write("{:.1f} {}".format(route.properties['length'], route.properties['units']))
+
+    st.write(dir(route))
+    
     import pandas as pd
     import numpy as np
 
