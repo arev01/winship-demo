bc3641e87c1af6682b412fbd28284c9c88ff1814
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-15 15:57:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,7 +91,7 @@ def main():
     # show route distance with unit
     st.write("{:.1f} {}".format(route.properties['length'], route.properties['units']))
 
-    st.write(dir(route))
+    st.write(route)
     
     import pandas as pd
     import numpy as np
