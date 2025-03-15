06621fdd7890054db9275b9005efd4ae22c4e0a4
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-15 16:38:21 +0100

    Create nodes.py

---
+++ nodes.py
@@ -0,0 +1,23 @@
+nodes = [
+    "Abidjan": (5.3252258, -4.0196030),
+    #"Bergen": 
+    "Busan": (35.1731121, 129.0714122),
+    "Constanta": (44.1759147, 28.6519359),
+    #"Gdansk": 
+    #"Hong Kong": 
+    #"Jeddah": 
+    #"Los Angeles": 
+    #"Marseille": 
+    #"Miami": 
+    "Mumbai": (19.0759837, 72.8776559),
+    #"New York": 
+    #"Ras Tanura": 
+    #"Rotterdam":  
+    #"Sao Paulo": 
+    #"Shanghai": 
+    #"Singapore": 
+    #"Sydney": 
+    #"Tokyo": 
+    #"Trieste": 
+    #"Turku": 
+    ]
