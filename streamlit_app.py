75d36ae98035a6d229bbf4aacba5f1644eaeff68
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-16 07:48:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,6 +92,26 @@ def main():
     )
     st.map(df, height=300)
 
+    # Use a maritime network geograph
+    from scgraph.geographs.marnet import marnet_geograph
+
+    # Get the shortest path between 
+    output = marnet_geograph.get_shortest_path(
+        #origin_node={"latitude": 31.23, "longitude": 121.47}, 
+        #destination_node={"latitude": 32.08,"longitude": -81.09}
+        origin_node={"latitude": nodes[origin][0], "longitude": nodes[origin][1]}, 
+        destination_node={"latitude": nodes[destination][0],"longitude": nodes[destination][1]}
+    )
+    st.write('Length: ',output['length']) #=> Length:  19596.4653
+    st.write(str([[i['latitude'],i['longitude']] for i in output['coordinate_path']]))
+
+    df2 = pd.DataFrame(
+        #np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+        output['coordinate_path'],
+        columns=["latitude", "longitude"],
+    )
+    st.map(df2, height=300)
+
     st.divider()
 
     buttons = [
