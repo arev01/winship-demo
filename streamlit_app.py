00358b235f42d47907c0174413df6ed51f330bbb
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-17 12:40:31 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,17 +42,25 @@ def st_horizontal():
 def greet(person):
     match person:
         case "container":
-            st.write("A ship specifically designed to carry dry cargo in intermodal containers.")
+            st.write("""A ship specifically designed 
+            to carry dry cargo in intermodal containers.""")
         case "cargo":
-            st.write("A multi-purpose ship designed to transport a wide variety of goods and commodities.")
+            st.write("""A multi-purpose ship designed 
+            to transport a wide variety of goods and commodities.""")
         case "tanker":
-            st.write("A ship specifically designed to carry liquid cargo, including petroleum, chemicals and pressurized gases.")
+            st.write("""A ship specifically designed 
+            to carry liquid cargo, including petroleum, chemicals 
+            and pressurized gases.""")
         case "bulker":
-            st.write("A ship specifically designed to transport unpackaged bulk cargo such as grain, coal, ore, steel coils and cement.")
+            st.write("""A ship specifically designed 
+            to transport unpackaged bulk cargo such as grain, coal, 
+            ore, steel coils and cement.""")
         case "frigate":
-            st.write("A small, fast military ship used to protect other ships.")
+            st.write("""A small, fast military ship used 
+            to protect other ships.""")
         case "passenger":
-            st.write("A large ship designed to carry people on voyages for vacationing.")
+            st.write("""A large ship designed 
+            to carry people on voyages for vacationing.""")
 
 # ---- MAIN FUNCTION ----
 def main():
@@ -62,6 +70,8 @@ def main():
     
     # Display the output in Streamlit
     st.write("You selected: ", out.capitalize())
+
+    greet(out)
 
     st.divider()
 
