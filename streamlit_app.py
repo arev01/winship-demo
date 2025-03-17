08fffcec1eade1b53e1f524630fd681667460c1e
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-17 18:36:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ HORIZONTAL_STYLE = """
         flex-wrap: wrap;
         gap: 0.5rem;
         align-items: baseline;
-        justify-content: flex-end;
+        /*justify-content: flex-end;*/
     }
     /* Buttons and their parent container all have a width of 704px, which we need to override */
     div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) div {
