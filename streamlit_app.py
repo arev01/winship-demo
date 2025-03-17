24878654893abe00fe9df5c5f4038206e63635ab
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-17 12:37:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,21 @@ def st_horizontal():
         st.markdown('<span class="hide-element horizontal-marker"></span>', unsafe_allow_html=True)
         yield
 
+def greet(person):
+    match person:
+        case "container":
+            st.write("A ship specifically designed to carry dry cargo in intermodal containers.")
+        case "cargo":
+            st.write("A multi-purpose ship designed to transport a wide variety of goods and commodities.")
+        case "tanker":
+            st.write("A ship specifically designed to carry liquid cargo, including petroleum, chemicals and pressurized gases.")
+        case "bulker":
+            st.write("A ship specifically designed to transport unpackaged bulk cargo such as grain, coal, ore, steel coils and cement.")
+        case "frigate":
+            st.write("A small, fast military ship used to protect other ships.")
+        case "passenger":
+            st.write("A large ship designed to carry people on voyages for vacationing.")
+
 # ---- MAIN FUNCTION ----
 def main():
     #---- ONGOING PROJECTS ----
