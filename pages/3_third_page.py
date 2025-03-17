3e984812dbd2da5e4fade097b68baf49ef934978
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-17 22:31:11 +0100

    Create 3_third_page.py

---
+++ pages/3_third_page.py
@@ -0,0 +1,87 @@
+import streamlit as st
+from mycomponent import mycomponent
+from functions import greet
+from contextlib import contextmanager
+
+HORIZONTAL_STYLE = """
+<style class="hide-element">
+    /* Hides the style container and removes the extra spacing */
+    .element-container:has(.hide-element) {
+        display: none;
+    }
+    /*
+        The selector for >.element-container is necessary to avoid selecting the whole
+        body of the streamlit app, which is also a stVerticalBlock.
+    */
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) {
+        display: flex;
+        flex-direction: row !important;
+        flex-wrap: wrap;
+        gap: 0.5rem;
+        align-items: baseline;
+        justify-content: flex-end;
+    }
+    /* Buttons and their parent container all have a width of 704px, which we need to override */
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) div {
+        width: max-content !important;
+    }
+    /* Just an example of how you would style buttons, if desired */
+    /*
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) button {
+        border-color: red;
+    }
+    */
+</style>
+"""
+
+@contextmanager
+def st_horizontal():
+    st.markdown(HORIZONTAL_STYLE, unsafe_allow_html=True)
+    with st.container():
+        st.markdown('<span class="hide-element horizontal-marker"></span>', unsafe_allow_html=True)
+        yield
+
+# Button to switch page
+with st_horizontal():
+    if st.button("<"):
+        page_file = "./pages/2_second_page.py"
+        # Switch to the selected page
+        st.switch_page(page_file)
+
+from nodes import nodes
+
+#Define origin and destination ports
+tab1, tab2 = st.tabs(["Origin", "Destination"])
+
+with tab1:
+    origin = st.selectbox(
+        "Select port of origin:",
+        nodes.keys()
+    )
+with tab2:
+    destination = st.selectbox(
+        "Select port of destination:",
+        nodes.keys()
+    )
+
+# Use a maritime network geograph
+from scgraph.geographs.marnet import marnet_geograph
+
+col = ["latitude", "longitude"]
+
+# Get the shortest path between 
+output = marnet_geograph.get_shortest_path(
+    origin_node = {col[i]: nodes[origin][i] for i, _ in enumerate(col)},
+    destination_node = {col[i]: nodes[destination][i] for i, _ in enumerate(col)}
+)
+st.write("Distance: ",output['length'])
+
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+    #np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    output['coordinate_path'],
+    columns = col,
+)
+st.map(df, height=300)
