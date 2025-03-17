0ca9e27fa05026a6f4283f2fb0cb117a33a8ed50
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-17 21:57:10 +0100

    Create 2_second_page.py

---
+++ pages/2_second_page.py
@@ -0,0 +1,54 @@
+import streamlit as st
+from mycomponent import mycomponent
+from functions import greet
+from contextlib import contextmanager
+
+HORIZONTAL_STYLE = """
+<style class="hide-element">
+    /* Hides the style container and removes the extra spacing */
+    .element-container:has(.hide-element) {
+        display: none;
+    }
+    /*
+        The selector for >.element-container is necessary to avoid selecting the whole
+        body of the streamlit app, which is also a stVerticalBlock.
+    */
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) {
+        display: flex;
+        flex-direction: row !important;
+        flex-wrap: wrap;
+        gap: 0.5rem;
+        align-items: baseline;
+        /*justify-content: flex-end;*/
+    }
+    /* Buttons and their parent container all have a width of 704px, which we need to override */
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) div {
+        width: max-content !important;
+    }
+    /* Just an example of how you would style buttons, if desired */
+    /*
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) button {
+        border-color: red;
+    }
+    */
+</style>
+"""
+
+@contextmanager
+def st_horizontal():
+    st.markdown(HORIZONTAL_STYLE, unsafe_allow_html=True)
+    with st.container():
+        st.markdown('<span class="hide-element horizontal-marker"></span>', unsafe_allow_html=True)
+        yield
+
+# Button to switch page
+prev_page = st.button("<"))
+if prev_page:
+    # Switch to the selected page
+    page_file = "./pages/1_first_page.py"
+    st.switch_page(page_file)
+next_page = st.button(">"))
+if next_page:
+    # Switch to the selected page
+    page_file = "./pages/1_first_page.py"
+    st.switch_page(page_file)
