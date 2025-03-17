55bbc6d64a5ea3c28f6edc28123b598745955b13
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-17 22:29:40 +0100

    Update 2_second_page.py

--- pages/2_second_page.py
+++ pages/2_second_page.py
@@ -48,7 +48,7 @@ with st_horizontal():
         # Switch to the selected page
         st.switch_page(page_file)
     if st.button("\>"):
-        page_file = "./pages/1_first_page.py"
+        page_file = "./pages/3_third_page.py"
         # Switch to the selected page
         st.switch_page(page_file)
 
@@ -56,41 +56,3 @@ option = st.selectbox(
     "Wind assisted device:",
     ("Wing", "Rotor", "Sail", "Kite"),
 )
-
-from nodes import nodes
-
-#Define origin and destination ports
-tab1, tab2 = st.tabs(["Origin", "Destination"])
-
-with tab1:
-    origin = st.selectbox(
-        "Select port of origin:",
-        nodes.keys()
-    )
-with tab2:
-    destination = st.selectbox(
-        "Select port of destination:",
-        nodes.keys()
-    )
-
-# Use a maritime network geograph
-from scgraph.geographs.marnet import marnet_geograph
-
-col = ["latitude", "longitude"]
-
-# Get the shortest path between 
-output = marnet_geograph.get_shortest_path(
-    origin_node = {col[i]: nodes[origin][i] for i, _ in enumerate(col)},
-    destination_node = {col[i]: nodes[destination][i] for i, _ in enumerate(col)}
-)
-st.write("Distance: ",output['length'])
-
-import pandas as pd
-import numpy as np
-
-df = pd.DataFrame(
-    #np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    output['coordinate_path'],
-    columns = col,
-)
-st.map(df, height=300)
