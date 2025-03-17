a6f64da567c8fd8c987114651ffb3d15522a551a
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-17 12:41:56 +0100

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -1,15 +1,15 @@
 nodes = {
-    "Abidjan": [5.3252258, -4.0196030],
+    "Abidjan": (5.3252258, -4.0196030),
     #"Bergen": 
-    "Busan": [35.1731121, 129.0714122],
-    "Constanta": [44.1759147, 28.6519359],
+    "Busan": (35.1731121, 129.0714122),
+    "Constanta": (44.1759147, 28.6519359),
     #"Gdansk": 
     #"Hong Kong": 
     #"Jeddah": 
     #"Los Angeles": 
     #"Marseille": 
     #"Miami": 
-    "Mumbai": [19.0759837, 72.8776559],
+    "Mumbai": (19.0759837, 72.8776559),
     #"New York": 
     #"Ras Tanura": 
     #"Rotterdam":  
