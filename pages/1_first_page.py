7cdd88b7e0b881144705abcc2eb07304484afbb2
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-17 21:53:25 +0100

    Update 1_first_page.py

--- pages/1_first_page.py
+++ pages/1_first_page.py
@@ -1,28 +1,52 @@
 import streamlit as st
 from mycomponent import mycomponent
+from functions import greet
+from contextlib import contextmanager
 
-def greet(person):
-    match person:
-        case "container":
-            st.write("""A ship specifically designed 
-            to carry dry cargo in intermodal containers.""")
-        case "cargo":
-            st.write("""A multi-purpose ship designed 
-            to transport a wide variety of goods and commodities.""")
-        case "tanker":
-            st.write("""A ship specifically designed 
-            to carry liquid cargo, including petroleum, chemicals 
-            and pressurized gases.""")
-        case "bulker":
-            st.write("""A ship specifically designed 
-            to transport unpackaged bulk cargo such as grain, coal, 
-            ore, steel coils and cement.""")
-        case "frigate":
-            st.write("""A small, fast military ship used 
-            to protect other ships.""")
-        case "passenger":
-            st.write("""A large ship designed 
-            to carry people on voyages for vacationing.""")
+HORIZONTAL_STYLE = """
+<style class="hide-element">
+    /* Hides the style container and removes the extra spacing */
+    .element-container:has(.hide-element) {
+        display: none;
+    }
+    /*
+        The selector for >.element-container is necessary to avoid selecting the whole
+        body of the streamlit app, which is also a stVerticalBlock.
+    */
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) {
+        display: flex;
+        flex-direction: row !important;
+        flex-wrap: wrap;
+        gap: 0.5rem;
+        align-items: baseline;
+        /*justify-content: flex-end;*/
+    }
+    /* Buttons and their parent container all have a width of 704px, which we need to override */
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) div {
+        width: max-content !important;
+    }
+    /* Just an example of how you would style buttons, if desired */
+    /*
+    div[data-testid="stVerticalBlock"]:has(> .element-container .horizontal-marker) button {
+        border-color: red;
+    }
+    */
+</style>
+"""
+
+@contextmanager
+def st_horizontal():
+    st.markdown(HORIZONTAL_STYLE, unsafe_allow_html=True)
+    with st.container():
+        st.markdown('<span class="hide-element horizontal-marker"></span>', unsafe_allow_html=True)
+        yield
+
+# Button to switch page
+next_page = st.button(">"))
+if next_page:
+    # Switch to the selected page
+    page_file = "./pages/2_second_page.py"
+    st.switch_page(page_file)
 
 out = mycomponent(my_input_value=50)
     
