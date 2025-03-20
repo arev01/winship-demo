97aee83cfb5e9bc1e5a85934113d69cfb8b980b3
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-20 13:36:32 +0100

    Update 2_second_page.py

--- pages/2_second_page.py
+++ pages/2_second_page.py
@@ -9,8 +9,23 @@ option = st.segmented_control("Wind assisted device:",
     ["Rotor", "Suction", "Wing", "Sail", "Kite"],
 )
 
-col1, col2 = st.columns(2)
-with col1:
-    st.image("./img/" + option.lower() + ".jpg", width=200)
-with col2:
-    greet(option.lower())
+with st.container(height=393, border=True):
+    cols_mr = st.columns([10.9, 0.2, 10.9])
+    with cols_mr[0].container(height=350, border=False):
+        st.write("Streamlit is an open-source Python framework for data scientists and AI/ML engineers to deliver dynamic data apps with only a few lines of code. Build and deploy powerful data apps in minutes. Let's get started!")
+        st.image("./img/" + option.lower() + ".jpg")
+    with cols_mr[1]:
+        st.html(
+            '''
+                <div class="divider-vertical-line"></div>
+                <style>
+                    .divider-vertical-line {
+                        border-left: 2px solid rgba(49, 51, 63, 0.2);
+                        height: 320px;
+                        margin: auto;
+                    }
+                </style>
+            '''
+        )
+    with cols_mr[2].container(height=350, border=False):
+        greet(option.lower())
