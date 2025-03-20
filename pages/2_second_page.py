e07a08668cc3505bbec3dbc486d9768e47f3f35c
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-20 08:24:28 +0100

    Update 2_second_page.py

--- pages/2_second_page.py
+++ pages/2_second_page.py
@@ -61,7 +61,7 @@ option = st.segmented_control("Wind assisted device:",
 )
 
 col1, col2 = st.columns(2)
-with col1.container(height=150, border=False):
-    st.image("./img/" + option.lower() + ".jpg")
-with col2.container(height=150, border=False):
+with col1:
+    st.image("./img/" + option.lower() + ".jpg", height=150)
+with col2:
     greet(option.lower())
