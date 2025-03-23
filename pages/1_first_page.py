316e4ae6b905c63ff4daa32bfe72c23ac0a7fe4b
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-23 13:35:22 +0100

    Update 1_first_page.py

--- pages/1_first_page.py
+++ pages/1_first_page.py
@@ -5,18 +5,22 @@ from navigation import menu
 
 menu(counter=1)
 
-out = mycomponent(my_input_value=50)
+my_output_value = mycomponent(my_input_value=50)
     
 # Display the output in Streamlit
-st.write("You selected: ", out.capitalize())
+st.write("You selected: ", my_output_value.capitalize())
 
-greet(out)
+greet(my_output_value)
 
 from pyresis import read_ship, ship
 
-file_name = out + ".shp"
+file_name = my_output_value + ".shp"
 
 out = read_ship.open(file_name)
 ship = ship.Ship(**out.values())
 
-st.write
+length = st.number_input("meters length", value=ship.length)
+beam = st.number_input("meters beam", value=ship.beam)
+draft = st.number_input("meters draft", value=ship.draft)
+speed = st.number_input("knots speed", value=ship.speed)
+
