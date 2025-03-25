32a6796a64b608602d6daed6bd8afdd24e5f5b67
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-25 16:31:49 +0100

    Update 2_second_page.py

--- pages/2_second_page.py
+++ pages/2_second_page.py
@@ -9,6 +9,16 @@ option = st.selectbox("Wind assisted device:",
     ["Rotor", "Suction", "Wing", "Sail", "Kite"],
 )
 
+# HTML to add an image to a button
+button_html = """
+<a href='https://www.example.com' target='_blank'>
+  <img src='./img/kite.jpg' alt='Button Image' style='width:100px;height:50px;'>
+</a>
+"""
+
+# Display the button with an image
+st.markdown(button_html, unsafe_allow_html=True)
+
 col1, col2 = st.columns(2)
 with col1:
     st.image("./img/" + option.lower() + ".jpg", width=40)
