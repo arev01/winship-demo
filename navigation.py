a1ef27ec51793985eb5cdda77c6c1130a980eeec
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-27 22:29:30 +0100

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -42,9 +42,9 @@ def st_horizontal():
 @st.dialog("You saved")
 def vote(varA, varB):
     col1, col2, col3 = st.columns(3)
-    col1.metric("Temperature", "70 °F", "1.2 °F")
-    col2.metric("Wind", "9 mph", "-8%")
-    col3.metric("Humidity", "86%", "4%")
+    col1.metric("Power", "70 kW", "1.2%")
+    col2.metric("Fuel", "9 L", "-8%")
+    col3.metric("CO2", "86 T", "4%")
 
 # Button to switch page
 def menu(counter):
