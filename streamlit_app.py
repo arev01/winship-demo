b3590cc7d5b2bed679d1cccc797208bfbe15fb22
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-29 07:42:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,23 +15,23 @@ st.markdown(
     
 # ---- MAIN FUNCTION ----
 def main():
-    st.title("Welcome to winship! 👋")
+    st.title("👋 Welcome to winship!")
     st.write(
         """
-        A tool that estimates the performance of a ship equipped with wind powered devices in 3 simple steps: select ship type & size, choose device configuration and select a course.
+        A tool that estimates the performance of a SHIP equipped with WINd powered devices in 3 simple steps: select ship type & size, choose device configuration and select a course.
 
         Accurate results plus a graphical output will be available in just a few seconds.
-
-        The code works by balancing the aerodynamic and hydrodynamic forces of the ship. It takes into account the most important eﬀects of the wind powered devices to predict the ship speeds at various wind conditions.
-
-        By determining the elapsed time around the course, a direct measure of the fuel and emission savings can be assessed.
         """
     )
     # Button to switch page
-    next_page = st.button("Start")
+    next_page = st.button("Start now")
     if next_page:
         # Switch to the selected page
         page_file = "./pages/1_first_page.py"
+        st.switch_page(page_file)
+    previous_page = st.button("Background")
+    if previous_page:
+        page_file = "./pages/4_fourth_page.py"
         st.switch_page(page_file)
 
 if __name__=='__main__':
