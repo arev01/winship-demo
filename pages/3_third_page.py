3bcefc1f85e01ae837453f481fecd37cf1ff2a5f
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-29 05:30:43 +0100

    Update 3_third_page.py

--- pages/3_third_page.py
+++ pages/3_third_page.py
@@ -42,19 +42,10 @@ data = xr.open_dataset('ERA5.nc')
 lat = data.latitude
 lon = data.longitude
 
-# Define the u-, v- wind speeds
-wind_u = data.u10
-wind_v = data.v10
-
-import numpy as np
-
-# Get the magnitude of wind speed
-WS = np.sqrt(wind_u **2 + wind_v **2)
-
 st.write(lat.shape)
 st.write(lon.shape)
-st.write(wind_u.shape)
-st.write(wind_v.shape)
+st.write(dir(data))
+st.write(data)
 
 import pandas as pd
 import numpy as np
