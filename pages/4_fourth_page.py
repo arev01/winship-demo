8679192d9eb97d1c2b963239d53110255b771eaf
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-29 07:46:29 +0100

    Update 4_fourth_page.py

--- pages/4_fourth_page.py
+++ pages/4_fourth_page.py
@@ -9,10 +9,10 @@ st.markdown(
         By determining the elapsed time around the course, a direct measure of the fuel and emission savings can be assessed.
 
         """
-        )
+)
 
-        previous_page = st.button("Go back")
-        if previous_page:
-                page_file = "./streamlit_app.py"
-                st.switch_page(page_file)
+previous_page = st.button("Go back")
+if previous_page:
+        page_file = "./streamlit_app.py"
+        st.switch_page(page_file)
         
