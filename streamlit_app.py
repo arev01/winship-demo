a8e7b6edd5cbe63351a9145ae0fd836b2b9bdaff
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-03-29 07:59:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,17 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
+
+@st.dialog("❓ Help")
+def help():
+    st.markdown(
+        """
+        Use the buttons :material/arrow_back_ios: :material/arrow_forward_ios:
+        to navigate the different menus or use :material/home: to go back.
+        Press X to evaluate your favorite design.
+        """
+    )
+
     
 # ---- MAIN FUNCTION ----
 def main():
@@ -29,6 +40,7 @@ def main():
         # Switch to the selected page
         page_file = "./pages/1_first_page.py"
         st.switch_page(page_file)
+        help()
     previous_page = st.button("Background")
     if previous_page:
         page_file = "./pages/4_fourth_page.py"
