78fdea5d7f6de454a35c03c8f029e81ae37b500c
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-04-05 22:28:28 +0200

    Update 2_second_page.py

--- pages/2_second_page.py
+++ pages/2_second_page.py
@@ -17,3 +17,34 @@ st.markdown("You selected: " + img[6:-4].capitalize())
 
 with st.expander("Description"):
     prompt(img[6:-4])
+
+import numpy as np
+
+# Get lift and drag coefficients
+lift_coef, drag_coef = np.loadtxt(img[6:-4] + ".dat"), comments="#", delimiter="\t", usecols=(1, 2), unpack=False)
+
+# Choose units, size 
+if img[6:-4] == "kite":
+    sizes_list = [
+        "300", "500", "1000"
+    ]
+else:
+    sizes_list = [
+        "18x3", "24x4", "30x5"
+    ]
+
+if ship.gross_tonnage < 10000:
+    units, size = 1, sizes_list[0]
+elif ship.gross_tonnage >= 10000 and ship.gross_tonnage < 50000:
+    units, size = 2, sizes_list[1]
+elif ship.gross_tonnage >= 50000 and ship.gross_tonnage < 100000:
+    units, size = 4, sizes_list[1]
+else:
+    units, size = 4, sizes_list[2]
+
+choice = st.segmented_control(["Quantity", "Dimension"], help=…)
+
+if choice == "Quantity":
+    st.number_input("Units (-):", [1, 2, 4], default=units, disabled=True)
+else:
+    st.dropdown_select("Size (m2):", sizes_list, default=size, disabled=True)
