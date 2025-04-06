aefc06e78f31e13403bcc5997eb6be0f873108e7
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-04-06 22:31:37 +0200

    Update wind.py

--- pyaero/wind.py
+++ pyaero/wind.py
@@ -4,6 +4,7 @@ class Wind():
     """
     Class of wind object, can be initialize with zero argument.
     """
+    
     def __init__(self, **kw) -> None:
         """
         Write something here
@@ -11,19 +12,29 @@ class Wind():
         self.__keys = {}
         self.__keys.update(kw)
 
-    def units(self, value):
-        return self.__keys['units'] = value
+    @property
+    def units(self):
+        return self.__keys['units']
 
+    @units.setter
+    def units(self, value):
+        self.__keys['units'] = value
+
+    @property
+    def size(self):
+        return self.__keys['size']
+
+    @sizer.setter
     def size(self, value):
-        return self.__keys['size'] = value
+        self.__keys['size'] = value
 
     def aero_force(self, speed: float, angle: float) -> float:
         """
-        Return aero force of the
+        Return aero force of the vehicle.
         """
 
         aero_coef = np.max(
-          self.drag_coef * np.cos(angle) 
-          - self.lift_coef * np.sin(angle))
+          self.drag_coef * np.cos(angle) - \
+          self.lift_coef * np.sin(angle))
 
         return 1 / 2 * aero_coef * 1.2 * self.units * self.size * speed ** 2
