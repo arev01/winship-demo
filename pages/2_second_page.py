2d134cec083146a128eef3d5b8788959c0c5124f
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-04-06 21:37:50 +0200

    Update 2_second_page.py

--- pages/2_second_page.py
+++ pages/2_second_page.py
@@ -33,11 +33,13 @@ else:
         "18x3", "24x4", "30x5"
     ]
 
-if ship.gross_tonnage < 10000:
+if st.session_state['ship'].gross_tonnage < 10000:
     units, size = 1, sizes_list[0]
-elif ship.gross_tonnage >= 10000 and ship.gross_tonnage < 50000:
+elif st.session_state['ship'].gross_tonnage >= 10000 \
+and st.session_state['ship'].gross_tonnage < 50000:
     units, size = 2, sizes_list[1]
-elif ship.gross_tonnage >= 50000 and ship.gross_tonnage < 100000:
+elif st.session_state['ship'].gross_tonnage >= 50000 \
+and st.session_state['ship'].gross_tonnage < 100000:
     units, size = 4, sizes_list[1]
 else:
     units, size = 4, sizes_list[2]
