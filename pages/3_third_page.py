f87c154b5a4ac1267762ae52cd6f3066ca40fbcc
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-04-07 16:40:22 +0200

    Update 3_third_page.py

--- pages/3_third_page.py
+++ pages/3_third_page.py
@@ -81,7 +81,7 @@ for i in range(len(marnet_output['coordinate_path'])-1):
 
     # Construct speed vectors
     v0 = np.asarray([boat_u, boat_v], dtype=float)
-    v1 = np.asarray([wind_u[xi, yi], wind_v[xi, yi], dtype=float)
+    v1 = np.asarray([wind_u[xi, yi], wind_v[xi, yi]], dtype=float)
 
     distance = navigation.distance(p1, p2)
 
