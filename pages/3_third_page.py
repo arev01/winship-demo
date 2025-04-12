e4d8646464edcae528c38b608e81ff75983373a9
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-04-12 23:38:27 +0200

    Update 3_third_page.py

--- pages/3_third_page.py
+++ pages/3_third_page.py
@@ -3,6 +3,29 @@ from functions import menu, prompt
 
 menu(counter=3)
 
+st.markdown("### (3/3) Select a route")
+
+import xarray as xr
+import numpy as np
+import pandas as pd
+
+# Open the ERA-5 data
+data = xr.open_dataset('ERA5.nc')
+
+# Define latitude and longitude coordinates
+lat = data.latitude[::3]
+lon = data.longitude[::3]
+
+# Define the u-, v- wind speeds
+wind_u = data.u[0,0,::3,::3]
+wind_v = data.v[0,0,::3,::3]
+
+def find_index(x, y):
+    global lat, lon
+    xi = np.searchsorted(lat, x)
+    yi = np.searchsorted(lon, y)
+    return xi, yi
+
 from nodes import nodes
 
 #Define origin and destination ports
@@ -25,69 +48,48 @@ from scgraph.geographs.marnet import marnet_geograph
 
 col = ["latitude", "longitude"]
 
-# Get the shortest path between 
-marnet_output = marnet_geograph.get_shortest_path(
-    origin_node = {col[i]: nodes[origin][i] for i, _ in enumerate(col)},
-    destination_node = {col[i]: nodes[destination][i] for i, _ in enumerate(col)},
-    output_units = "km"
-)
-st.write("Distance: " + str(marnet_output['length']) + " km")
+if origin =! '<select>' and destination =! '<select>':
+    # Get the shortest path between 
+    marnet_output = marnet_geograph.get_shortest_path(
+        origin_node = {col[i]: nodes[origin][i] for i, _ in enumerate(col)},
+        destination_node = {col[i]: nodes[destination][i] for i, _ in enumerate(col)},
+        output_units = "km"
+    )
+    st.write("Distance: " + str(marnet_output['length']) + " km")
 
-import pandas as pd
+    df = pd.DataFrame(
+        #np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+        marnet_output['coordinate_path'],
+        columns = col,
+    )
+    st.map(df, height=300)
 
-df = pd.DataFrame(
-    #np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    marnet_output['coordinate_path'],
-    columns = col,
-)
-st.map(df, height=300)
+    st.toggle("Show wind", disabled=True, help="Currently unavailable")
 
-st.toggle("Show wind", disabled=True, help="Currently unavailable")
+    from pyaero import navigation
 
-import xarray as xr
-import numpy as np
+    lst = []
+    for i in range(len(marnet_output['coordinate_path'])-1):
+        p1 = marnet_output['coordinate_path'][i]
+        p2 = marnet_output['coordinate_path'][i+1]
 
-# Open the ERA-5 data
-data = xr.open_dataset('ERA5.nc')
+        if p1[0] == p2[0]:
+            boat_u = st.session_state['ship'].speed1
+            boat_v = 0
+        else:
+            X = ( p1[1] - p2[1] ) / ( p1[0] - p2[0] )
+            boat_u = st.session_state['ship'].speed1 / np.sqrt(1 + X**2) * X
+            boat_v = st.session_state['ship'].speed1 / np.sqrt(1 + X**2)
 
-# Define latitude and longitude coordinates
-lat = data.latitude[::3]
-lon = data.longitude[::3]
+        xi, yi = find_index(*p1)
 
-# Define the u-, v- wind speeds
-wind_u = data.u[0,0,::3,::3]
-wind_v = data.v[0,0,::3,::3]
-
-def find_index(x, y):
-    global lat, lon
-    xi = np.searchsorted(lat, x)
-    yi = np.searchsorted(lon, y)
-    return xi, yi
-
-from pyaero import navigation
-
-lst = []
-for i in range(len(marnet_output['coordinate_path'])-1):
-    p1 = marnet_output['coordinate_path'][i]
-    p2 = marnet_output['coordinate_path'][i+1]
-
-    if p1[0] == p2[0]:
-        boat_u = st.session_state['ship'].speed1
-        boat_v = 0
-    else:
-        X = ( p1[1] - p2[1] ) / ( p1[0] - p2[0] )
-        boat_u = st.session_state['ship'].speed1 / np.sqrt(1 + X**2) * X
-        boat_v = st.session_state['ship'].speed1 / np.sqrt(1 + X**2)
-
-    xi, yi = find_index(*p1)
-
-    # Construct speed vectors
-    v0 = np.asarray([boat_u, boat_v], dtype=float)
-    v1 = np.asarray([wind_u[yi, xi], wind_v[yi, xi]], dtype=float)
+        # Construct speed vectors
+        v0 = np.asarray([boat_u, boat_v], dtype=float)
+        v1 = np.asarray([wind_u[yi, xi], wind_v[yi, xi]], dtype=float)
     
-    lst.append([navigation.distance(*p1, *p2), *navigation.velocity(v0, v1)])
+        lst.append([navigation.distance(*p1, *p2), *navigation.velocity(v0, v1)])
     
-wind_data = pd.DataFrame(lst, columns=['DIST', 'TWS', 'TWA', 'AWS', 'AWA'])
+    wind_data = pd.DataFrame(lst, columns=['DIST', 'TWS', 'TWA', 'AWS', 'AWA'])
 
 if 'updated_data' not in st.session_state:
     st.session_state['updated_data'] = wind_data
