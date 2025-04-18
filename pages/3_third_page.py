1a5b82d5889329d5bd5e04706be1a1e424e28406
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-04-18 21:39:46 +0200

    Update 3_third_page.py

--- pages/3_third_page.py
+++ pages/3_third_page.py
@@ -97,8 +97,9 @@ if origin != '<select>' and destination != '<select>':
         v0 = np.asarray([boat_u, boat_v], dtype=float)
         v1 = np.asarray([wind_u[xi, yi], wind_v[xi, yi]], dtype=float)
 
-        st.write(v0, v1)
-        st.write(*navigation.velocity(v0, v1))
+        st.write(boat_u, boat_v)
+        st.write(v0)
+        #st.write(*navigation.velocity(v0, v1))
         st.write()
     
         lst.append([navigation.distance(*p1, *p2), *navigation.velocity(v0, v1)])
