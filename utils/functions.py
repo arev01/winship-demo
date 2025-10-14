34ee6493c31494702285cdfb8624be48b540146d
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-10-14 08:17:04 +0200

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -145,19 +145,17 @@ def predict(varA, varB):
             col2.metric("Fuel savings", '{0:,.0f} L'.format(diff_fuel), '{0:.1f} %'.format(pc_fuel))
             col3.metric("Emission savings", '{0:,.0f} TCO2e'.format(diff_emissions), '{0:.1f} %'.format(pc_emissions))
 
-    st.dataframe(st.session_state['ship'])
-    st.write(st.session_state['ship'].gross_tonnage, st.session_state['ship'].length, st.session_state['ship'].beam, st.session_state['ship'].draft, st.session_state['ship'].speed)
-    
     import pandas as pd
     
     confusion_matrix = pd.DataFrame(
         {
-            "Predicted Cat": [85, 3, 2, 1],
-            "Predicted Dog": [2, 78, 4, 0],
-            "Predicted Bird": [1, 5, 72, 3],
-            "Predicted Fish": [0, 2, 1, 89],
+            "Predicted Cat": [st.session_state['ship'].gross_tonnage,
+                             st.session_state['ship'].length,
+                             st.session_state['ship'].beam,
+                             st.session_state['ship'].draft,
+                             st.session_state['ship'].speed],
         },
-        index=["Actual Cat", "Actual Dog", "Actual Bird", "Actual Fish"],
+        index=["Gross Tonnage", "Length", "Beam", "Draft", "Speed"],
     )
     st.table(confusion_matrix)
 
