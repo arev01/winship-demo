fa054a9f23392ce11b6d58e25a0d2c05f591c5a7
Antoine Reverberi <106743110+arev01@users.noreply.github.com>
2025-10-14 08:06:03 +0200

    Update read_ship.py

--- lib/pyhydro/read_ship.py
+++ lib/pyhydro/read_ship.py
@@ -2,6 +2,7 @@ _open = open
 
 
 def open(file=None):
+    
     out = {}
     try:
         f = _open(file, "r")
@@ -10,12 +11,16 @@ def open(file=None):
 
     lines = f.readlines()
     for line in lines:
-        line = line.strip().split("#")[0].strip()
+        line = line.strip().split('#')[0].strip()
+        
         if len(line) > 0:
             res = line.split("=")
             key, value = res[0].strip(), res[1].strip()
             out[key] = float(value)
             #self.__keys.append(key)
+    
+    out['type'] = file.split('/')[-1].split('.')[0]
+    
     return out
 
 def edit(file=None):
